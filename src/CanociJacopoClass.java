02346532107ad406aff4f4ecf02b16a4e46291fc
CanociJacopo <jacopocano@gmail.com>
2022-05-04 10:01:21 +0200

    Versione 1: corretta

--- src/CanociJacopoClass.java
+++ src/CanociJacopoClass.java
@@ -9,7 +9,7 @@
  *         <li>canociMet(int s, int f)
  *         </ol>
  *         </html>
- *         @version 0
+ * @version 1
  *
  */
 public class CanociJacopoClass {
@@ -25,14 +25,18 @@ public class CanociJacopoClass {
 
 	/**
 	 * Metodo che calcola il valore della produttoria tra due numeri
+	 * 
 	 * @param s Valore che indica il valore iniziale
 	 * @param f Valore che indica il valore finale
 	 * @return il valore della produttoria
 	 */
 	public int canociMet(int s, int f) {
-		int r = 0;
-		for (int i = 1; i < f; i++) {
-			r = f * i;
+		int r = (f * f) * (s * s);
+		for (int i = s + 1; i < f; i++) {
+			r *= i * i;
+			// all'interno di questo metodo gli errori sono: all'inizio dobbiamo mettere = a (f*f) * (s*s) per rispondere alla formula matematica
+			//il secondo errore è nel for dobbiamo mettere la i = s+1
+			//e l'ultimo errore sta dentro il for dobbiamo mettere l'istruzione uguale e moltiplicata a i*i
 		}
 		return r;
 	}
